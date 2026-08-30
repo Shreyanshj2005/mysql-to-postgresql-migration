3ea4884d87f6d7a4b17e61a1a1d3e47284aacd0a
Shreyanshj2005 <shreyansh.bottomhalf@gmail.com>
2026-08-30 23:58:12 +0530

    Add stored procedure sp_catagory_getby_filter
    Created a stored procedure to retrieve categories based on filters, with sorting and pagination.

---
+++ sp_catagory_getby_filter.sql
@@ -0,0 +1,45 @@
+CREATE OR REPLACE PROCEDURE public.sp_catagory_getby_filter(
+  IN _searchstring varchar(250),
+  IN _sortby varchar(50),
+  IN _pageindex integer,
+  IN _pagesize integer,
+    INOUT _refcur refcursor DEFAULT 'cur_result'
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+  _sqlstate TEXT;
+  _errorno TEXT;
+  _errortext TEXT;
+  _message TEXT;
+  _result TEXT;
+  _selectquery TEXT;
+BEGIN
+  begin
+ 
+ begin
+ if(_sortby is null or _sortby = '') then
+ _sortby := 'CategoryId';
+ end if;
+ _selectquery := concat('Select * from (
+  Select 
+  Row_Number() over(Order by ', _sortby, ') as RowIndex,
+  CategoryId,
+  GroupId,
+  CategoryCode,
+  CategoryDescription,
+  Count(1) Over() as Total from catagory
+  Where ', _searchstring, '
+  )T where RowIndex between ', ((_pageindex - 1 ) * _pagesize + 1), ' and ', (_pageindex * _pagesize));
+ 
+ 
+ OPEN _refcur FOR EXECUTE _selectquery;end;
+ end;
+EXCEPTION WHEN OTHERS THEN
+  _sqlstate := SQLSTATE;
+  _errortext := SQLERRM;
+  _errorno := SQLSTATE;
+  _message := concat('ERROR ', _errorno, ' (', _sqlstate, '): ', _errortext);
+  CALL public.sp_logexception(_message, '', 'sp_catagory_getby_filter', 1::bit, 0::bit, _result);
+END;
+$$;
